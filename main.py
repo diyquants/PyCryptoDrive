98fcbef12c5e45b26622cbe7a1e2227255ea83dd
diyquants <151967111+diyquants@users.noreply.github.com>
2025-06-01 11:33:27 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from pipeline import Pipe as pp
+import os
+#1.暗号化アルゴリズムの選定->DeepResearch
+#2.ファイルの分割->情報量最大化
+#3.圧縮アルゴリズムの選定->DeepResearch
+#4.ディレクトリ構造の保管形式(json/text)と暗号化方式
+#5.フロントエンド開発(HTML+CSS+JS+API)
---
+++ readme.md
@@ -0,0 +1,6 @@
+# what to do
+## 1.暗号化アルゴリズムの選定->DeepResearch
+## 2.ファイルの分割->情報量最大化
+## 3.圧縮アルゴリズムの選定->DeepResearch
+## 4.ディレクトリ構造の保管形式(json/text)と暗号化方式
+## 5.フロントエンド開発(HTML+CSS+JS+API)
