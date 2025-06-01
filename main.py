168a7a392ec35678acf816eed7b8ae0cce462c25
diyquants <151967111+diyquants@users.noreply.github.com>
2025-06-01 16:44:40 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from pipeline import Pipe as pp
 import os
-#1.暗号化アルゴリズムの選定->AES256
 
+#絶対修正必要
+def traverse(dir,paths=[]):
+	for line in os.listdir(dir):
+		if type(line)=="directory":
+			return traverse([]+line)
+		else:
+			paths.append(line)
+	return paths
+#疑似コード、絶対修正必要
 with open(file,"rb") as h:
 	file={}
 	while True:
@@ -13,7 +21,7 @@ with open(file,"rb") as h:
 			encryption(tmp)
 			counter+=1
 			file[f"file_{counter}"]={#ディレクトリとかタイムスタンプとか？#}
-			
+#1.暗号化アルゴリズムの選定->AES256			
 #2.ファイルの分割->情報量最大化->効率を考えて50MB
 #3.圧縮アルゴリズムの選定->zstd or lz4
 #4.ディレクトリ構造の保管形式(json)と暗号化方式(AES)
--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 # what to do
 ### CryptoMatorに不満があるので、Client-Side Crypto Containerを自作する
-## 1.暗号化アルゴリズムの選定->AES256GCM
-## 2.ファイルの分割->情報量最大化->効率を考えて10-50MB/ユーザ指定
-## 3.圧縮アルゴリズムの選定->zstd
-## 4.ディレクトリ構造の保管形式(jsonl)と暗号化方式(AES256GCM)
-## 5.フロントエンド開発(HTML+CSS+JS+API or Python+HTML+CSS)
+## 1.暗号化アルゴリズムの選定->AES256
+## 2.ファイルの分割->情報量最大化->効率を考えて50MB
+## 3.圧縮アルゴリズムの選定->zstd or lz4
+## 4.ディレクトリ構造の保管形式(jsonl)と暗号化方式(AES)
+## 5.フロントエンド開発(HTML+CSS+JS+API)
     - Drive>Folder1>Folder2
         - File1
         - Folder3
